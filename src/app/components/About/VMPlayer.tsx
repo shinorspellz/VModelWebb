cab1c2b9d1a4662bff267e97dc2553e6ccb8a5c6
Devdannnny <devdannny@gmail.com>
2023-11-28 15:02:41 +0000

    Video Player Fixed | Favicon Update

--- src/app/components/About/VMPlayer.tsx
+++ src/app/components/About/VMPlayer.tsx
@@ -31,9 +31,6 @@ const VMPlayer = () => {
 
   const _handlePlay = () => {
     setIsPlaying(!isPlaying);
-    // setTimeout(() => {
-    //   setIsPlaying(!isPlaying);
-    // }, 300);
   };
 
   return (
@@ -44,17 +41,22 @@ const VMPlayer = () => {
             <VMContainer>
               <div className="flex flex-col md:flex-row w-full mx-auto md:items-center md:pt-[80px] px-[10px]">
                 <div className="h-auto mb-12 md:mb-0 md:h-[80vh] w-full rounded-md overflow-hidden relative">
-                  <div className="absolute z-30 w-full h-full bg-black opacity-40"></div>
-                  <div className="absolute left-3 md:left-0 bottom-2 md:bottom-0 z-30">
-                    <div className="self-start xl:p-16 md:p-12">
-                      <div className="font-bold text-white text-lg md:text-3xl md:mb-3 richtext">
-                        Welcome
+                  {!isPlaying && (
+                    <>
+                      {" "}
+                      <div className="absolute z-30 w-full h-full bg-black opacity-40"></div>
+                      <div className="absolute left-3 md:left-0 bottom-2 md:bottom-0 z-30">
+                        <div className="self-start xl:p-16 md:p-12">
+                          <div className="font-medium sm:font-bold text-white -mb-1 text-[15px] md:text-3xl md:mb-3 richtext">
+                            Welcome
+                          </div>
+                          <p className="text-[12px] opacity-70 md:text-lg richtext text-white">
+                            Vmodel App
+                          </p>
+                        </div>
                       </div>
-                      <p className="text-base md:text-lg richtext text-white">
-                        Vmodel App
-                      </p>
-                    </div>
-                  </div>
+                    </>
+                  )}
 
                   <div className="md:absolute z-20 w-full rounded-md overflow-hidden h-full">
                     <ReactPlayer
@@ -90,12 +92,14 @@ const VMPlayer = () => {
                     // }}
                   >
                     <button
-                      className=" h-8 w-8 md:h-16 md:w-16 flex z-20 rounded-full border-white border place-items-center justify-center absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2"
+                      className={`h-8 w-8 transition-all duration-[.3s] mob-svg md:h-16 md:w-16 flex ${
+                        isPlaying ? "isPlaying" : ""
+                      } z-20 rounded-full border-white border place-items-center justify-center absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2`}
                       type="button"
                       onClick={_handlePlay}
-                      aria-label="Play Muck N Brass video"
+                      aria-label="VModel App Preview"
                     >
-                      {isPlaying ? (
+                      {!isPlaying ? (
                         <div className="w-0 h-0 border-y-[12px] scale-50 md:scale-95 border-y-transparent border-l-[20px] border-l-white ml-1"></div>
                       ) : (
                         <Pause size="32" color="#fff" variant="Bold" />
