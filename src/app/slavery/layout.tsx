fe965c0648a77f5a6ad951b77cb10a9b5e27d1a6
“rich” <adukyerer@gmail.com>
2024-04-16 09:12:33 +0000

    Add slavery Policy

---
+++ src/app/slavery/layout.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "About Us - VModel",
+    description:
+      "Learn more about VModel, our mission and how we are empowering both creatives and businesses to access top-tier talent, fostering meaningful collaborations that know no limits. ",
+  };
+  
+  export default function PolicyLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return <>{children}</>;
+  }
+  
