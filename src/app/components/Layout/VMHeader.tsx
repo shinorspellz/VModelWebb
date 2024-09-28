5f8b46f89e79551e1259d359e2b045fb02d794ab
“rich” <adukyerer@gmail.com>
2024-09-28 15:26:32 +0000

    Update Dashlinks 2

--- src/app/components/Layout/VMHeader.tsx
+++ src/app/components/Layout/VMHeader.tsx
@@ -72,7 +72,7 @@ const VMHeader = () => {
     },
     {
       id: 1,
-      url: "https://v-model-dash.vercel.app/",
+      url: "/dashboard",
       linkText: "For Employees",
       externalLinks: false,
     },
