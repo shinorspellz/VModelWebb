9ff552328a6261bf4b8281d0232c1da533771645
“rich” <adukyerer@gmail.com>
2024-10-14 20:32:45 +0000

    Remove metada

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -1,32 +1,10 @@
-// src/app/services/layout.tsx
 
-import { Metadata } from 'next';
-
-async function getServiceData(service_id: string): Promise<any | null> {
-  const res = await fetch(`/api/services?service_id=${service_id}`);
-  if (!res.ok) {
-    throw new Error('Failed to fetch service details');
-  }
-  const data = await res.json();
-  return data?.data?.service || null;
-}
-
-export async function generateMetadata({ params }: { params: { service_id: string } }): Promise<Metadata> {
-  const serviceData = await getServiceData(params.service_id);
-
-  return {
-    title: serviceData?.title || 'Service Details - Deluxe Motorcycle Engineering',
-    description: serviceData?.description || 'Explore the best motorcycle engineering services.',
-    openGraph: {
-      title: serviceData?.title || 'Service Details',
-      description: serviceData?.description || 'Check out our top-notch motorcycle services.',
-      images: serviceData?.user?.profilePictureUrl || '/default-image.png',
-      url: `https://vmodelapp.com/services?service_id=${params.service_id}`,
-      type: 'website',
-    },
-  };
-}
-
-export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+export default function GettingStarted({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <>{children}</>;
 }
+
+
