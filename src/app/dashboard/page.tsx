5d0a07824447959c3a35150e6348e3bb24f1da41
“rich” <adukyerer@gmail.com>
2024-11-14 11:31:23 +0000

    Add cookie fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,45 @@
-'use client'
+
+'use client';
 import React, { useEffect } from "react";
-import Iframe from "react-iframe";
 
 const Dashboard = () => {
+  useEffect(() => {
+    // Replace with the actual dashboard URL
+    window.location.replace("https://v-model-dash.vercel.app");
+  }, []);
 
-
-  return (
-    <Iframe
-      url="https://v-model-dash.vercel.app"
-      width="100%"
-      height="100%"
-      id="dashframe"
-      loading="lazy"
-      allowFullScreen
-      styles={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        border: "none", // Optional: Remove iframe border
-      }}
-    />
-  );
+  return null; // Render nothing as we're redirecting
 };
 
-export default Dashboard
+export default Dashboard;
+
+
+
+// 'use client'
+// import React, { useEffect } from "react";
+// import Iframe from "react-iframe";
+
+// const Dashboard = () => {
+
+
+//   return (
+//     <Iframe
+//       url="https://v-model-dash.vercel.app"
+//       width="100%"
+//       height="100%"
+//       id="dashframe"
+//       loading="lazy"
+//       allowFullScreen
+//       styles={{
+//         position: "fixed",
+//         top: 0,
+//         left: 0,
+//         width: "100%",
+//         height: "100%",
+//         border: "none", // Optional: Remove iframe border
+//       }}
+//     />
+//   );
+// };
+
+// export default Dashboard
