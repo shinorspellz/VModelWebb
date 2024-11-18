76a614ad5ca21f6fde35b6b9e6c8bbe3ef2ee973
“rich” <adukyerer@gmail.com>
2024-11-18 07:59:38 +0000

    Refactor image banner size in ServiceDetailsPost component

--- src/app/components/services/ServiceDetailsPost.tsx
+++ src/app/components/services/ServiceDetailsPost.tsx
@@ -78,7 +78,7 @@ const ServiceDetailsPost: React.FC<ServiceDetailsPostProps> = ({ data }) => {
                         {data.bannerUrl.map((banner: any, index: number) => (
                             <div
                                 key={banner.thumbnail}
-                                className="max-w-[300px] h-24 cursor-pointer rounded-lg overflow-hidden border border-gray-300 hover:border-primary hover:border-[3px] transition"
+                                className="w-[100px] h-24 cursor-pointer rounded-lg overflow-hidden border border-gray-300 hover:border-primary hover:border-[3px] transition"
                                 onClick={() => handleBannerClick(index)}
                             >
                                 <img
